24eaedd179b72537d8e0876299bdb069df0cd547
CY <cy823236858@gmail.com>
2024-09-18 10:07:22 +0800

    fix_typo

--- move_file_to_upload.py
+++ move_file_to_upload.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import json
-
+import argparse
 
 
 parser = argparse.ArgumentParser(description = "move file to single folder")
@@ -13,7 +13,7 @@ parser.add_argument('--size_thresh',              type=int,   default=224,   hel
 parser.add_argument('--single_face',              type=bool,   default=True,   help='whether single face in frame')
 parser.add_argument('--mode',              type=str,   default="single_video",   help='whether keep strcture or single video')
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
 def main():
     # upload_dir = 'NVAIE_Bili_24_09/'
@@ -23,10 +23,11 @@ def main():
     # size_threshold = 224
     # mode = "video_folder"
     # mode = "keep_structure"
-
+    args = parser.parse_args()
+    print(args)
     mode = args.mode
-    upload_dir = args.out_dir
-    processed_dir = arg.input_dir
+    upload_dir = args.out_dir + "/"
+    processed_dir = args.input_dir + "/"
     score_threshold = args.score_thresh
     size_threshold = args.size_thresh
     single_face_condition = args.single_face
@@ -77,10 +78,10 @@ def main():
             h = item[3]
             w = item[4]
             file_name = path.split('\\')[-1]
-            print(file_name, score, single_face,size)
+
             size = (h+w) /2
 
-
+            print(file_name, score, single_face, size)
             if score > score_threshold and size > size_threshold and (single_face or (not single_face_condition)):
                 if mode == "keep_structure":
 
